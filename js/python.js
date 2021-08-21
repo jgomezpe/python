effcb03b67b48e31c09170e043272fefe69a2463
Jonatan Gomez <jogoper@gmail.com>
2021-08-20 23:34:06 -0500

    Improves python.js component

--- js/python.js
+++ js/python.js
@@ -19,41 +19,55 @@
 class Python extends Client{
 	/**
 	 * Creates a Python server client
-	 * @param id GUI id
-	 * @param url Python server url
-	 * @param type If Python console will be displayed as a row ('row') or as a column ('col') 
-	 * @param captionRun Caption for the run button when ready for running python code (to start code running)
-	 * @param captionStop Caption for the run button when running python code (to stop code running)
+	 * @param config Configuration includes
+	 * id: GUI's id,
+	 * editor: Editor's id
+	 * url: Python's server url
+	 * run: Run button's id 
+	 * console: Console/Terminal's id
+	 * type If python console will be displayed as a row ('row') or as a column ('col') 
+	 * captionRun Caption for the run button when ready for running python code (to start code running)
+	 * captionStop Caption for the run button when running python code (to stop code running)
 	 */
-	constructor(id, url, type="col", captionRun='', captionStop=''){
-		super(id+"Python")
-		this.captionRun = captionRun
-		this.captionStop = captionStop
-		this.url = url
+	constructor(config){
+		super(config.id+"Python")
+		this.url = config.url
 		this.running = false
-		Konekti.split( 
-		  { "id":id, "type":type,
+		this.captionRun = config.captionRun || ''
+		this.captionStop = config.captionStop || ''
+		this.editorID = config.editor || 'coderPy'
+		this.consoleID = config.console || 'viewerPy'
+		var type = config.type || 'col'
+		var maxCharsConsole = config.maximum || 1000000
+		var initial = config.initial || ''
+		
+		var aceCfg = { "plugin":"ace","id":this.editorID,"mode":"python","initial":initial }
+		var split = { "id":config.id, "type":type,
 		  "start":60,
-		  "one":{
-		  	"plugin":"hcf", 
-		  	"content":{ "plugin":"ace","id":"coder","mode":"python","initial":"x=int(input('?'))\nprint(x+3)\n" },
-		  	"header":{"plugin":"btn","id":"run", "icon":"fa fa-play","caption":captionRun,
-		  	  "options":"w3-bar-item w3-medium","onclick":{"client":id+"Python"}}
-		  },
-		  "two":{"plugin":"terminal","id":"viewer","initial":""}
+		  "two":{"plugin":"terminal","id":this.consoleID,"initial":"","maximum":maxCharsConsole}
 		}
-		)
+		
+		this.btnID = config.run || 'runPy'
+		var btnCfg = {"plugin":"btn","id":this.btnID, "icon":"fa fa-play","caption":this.captionRun,
+		  	  "options":"w3-bar-item w3-medium","onclick":{"client":this.id, "method":"run"}}
+		var btn = Konekti.client( this.btnID )
+		if( btn === undefined || btn === null )
+		 split.one =  { "plugin":"hcf", "content": aceCfg, "header":btnCfg}
+		else{
+			btn.update(btnCfg)
+			split.one = aceCfg
+		}
+		Konekti.split( split )
 	}
 
 	/**
 	 * Function called when run button is pressed
-	 * @param btnId Run button id
 	 */
-	run( btnId ){
+	run(){
 		var x = this
-		var terminal = Konekti.client('viewer')
-		var editor = Konekti.client('coder')
-		var btn = Konekti.client('run')
+		var terminal = Konekti.client(x.consoleID)
+		var editor = Konekti.client(x.editorID)
+		var btn = Konekti.client(x.btnID)
 		if( x.running ){
 			x.server.end()
 			btn.update({"icon":"fa fa-play","caption":x.captionRun})
@@ -61,7 +75,7 @@ class Python extends Client{
 			terminal.init()
 			btn.update({"icon":"fa fa-stop","caption":x.captionStop})
 			x.server = new ProcessRunner(x.url,"PythonServer",[editor.getText()])
-			terminal.set(this.server)
+			terminal.set(x.server)
 			x.server.run( function(response){
 				if( response !== undefined ) terminal.output(response)
 				else{
